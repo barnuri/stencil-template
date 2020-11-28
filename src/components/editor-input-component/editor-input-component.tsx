c0e6e48a727f1dd311ffeec4b675d7ef4bbf8a8c
Bar Nuri <barn@ke-la.com>
2020-11-28 15:02:57 +0200

    Init

---
+++ src/components/editor-input-component/editor-input-component.tsx
@@ -0,0 +1,11 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'editor-input',
+    shadow: true,
+})
+export class EditorInputComponent {
+    render() {
+        return <div>Hello, World! I'm </div>;
+    }
+}
